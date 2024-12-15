08b5af19ab91ca2bbcfe65b181a2fe3710b33f62
Luc1pher13 <andreloureiro0018@gmail.com>
2024-12-15 15:14:04 +0000

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,37 @@
+'''
+x = 5
+y = "5"
+print(x + int(y))
+'''
+'''
+print(bool(0), bool(1), bool(""))
+'''
+'''
+dct = {}
+dct['1'] = (1,2)
+dct['2'] = (2,1)
+
+for x in dct.keys():
+    print(dct[x][1], end='')
+'''
+'''
+x = 3
+x **= 2
+print(x)
+'''
+'''
+x = "hello"
+print(x.upper())
+'''
+'''
+x = [1, 2, 3, 4, 5]
+print(x[-2])
+'''
+'''
+x = [1, 2, 3]
+x.insert(1, 5)
+print(x)
+'''
+
+my_string = "Python"
+print(len(my_string))
